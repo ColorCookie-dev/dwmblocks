48e5a54de94ad6f927afe697c6e4cf99e398139c
Color Cookie <colorcookie@protonmail.com>
2020-11-25 12:37:37 +0530

    more status commands added

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{"",		"diskspace",		30,					0},
+	{"",		"memory",			5,					1},
+	{"",		"dwmsound",			0,					10},
+	{"",		"dwmbrightness",	0,					12},
+	{"",		"battery",			0,					5},
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"",		"dwmclock",			5,					4},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
